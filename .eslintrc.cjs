9bc6d88ec16933a230e5b65f3293984949c801a0
Hayato Sakaguchi <hayato8190@gmail.com>
2024-01-07 10:34:26 +0900

    update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,5 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:vue/vue3-recommended',
-    'plugin:astro/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -23,11 +16,24 @@ module.exports = {
     },
     {
       files: ['*.astro'],
+      extends: ['plugin:astro/recommended'],
       parser: 'astro-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro'],
       },
+    },
+    {
+      files: ['*.jsx', '*.tsx'],
+      extends: ['plugin:react/recommended'],
+      rules: {
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+      },
+    },
+    {
+      files: ['*.vue'],
+      extends: ['plugin:vue/vue3-recommended'],
     },
   ],
 };
