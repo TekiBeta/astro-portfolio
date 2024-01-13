ac594c2c072363071e2c5da94737aff797a7df7a
Hayato Sakaguchi <hayato8190@gmail.com>
2024-01-13 21:58:17 +0900

    update changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@ import { PhHouseSimple, PhImages, PhInfo, PhList, PhNewspaper } from '@phosphor-
 
 <template>
   <header>
-    <div class="flex bg-dark h-16 px-4 [&>*:first-child]:mr-auto text-[#fff]">
+    <div class="flex h-16 bg-dark px-4 text-[#fff] [&>*:first-child]:mr-auto">
       <div class="logo flex items-center text-xl">
         <img src="../../public/astro-icon-light-gradient.svg" class="h-12 px-4" />
         <div class="">Website</div>
@@ -12,31 +12,31 @@ import { PhHouseSimple, PhImages, PhInfo, PhList, PhNewspaper } from '@phosphor-
 
       <!-- PC用 -->
       <nav class="hidden md:flex">
-        <div class="flex justify-center items-center px-2 w-32">
+        <div class="flex w-32 items-center justify-center px-2">
           <PhHouseSimple :size="20" />
           <span class="">HOME</span>
         </div>
-        <div class="flex justify-center items-center px-2 w-32">
+        <div class="flex w-32 items-center justify-center px-2">
           <PhImages :size="20" />
           <span>GALLERY</span>
         </div>
-        <div class="flex justify-center items-center px-2 w-32">
+        <div class="flex w-32 items-center justify-center px-2">
           <PhNewspaper :size="20" />
           <span>BLOG</span>
         </div>
-        <div class="flex justify-center items-center px-2 w-32">
+        <div class="flex w-32 items-center justify-center px-2">
           <PhInfo :size="20" />
           <span>ABOUT</span>
         </div>
       </nav>
 
       <!-- スマホ用 -->
-      <div class="flex md:hidden items-center">
+      <div class="flex items-center md:hidden">
         <div class="px-4">
           <PhList :size="32" />
         </div>
       </div>
-      <nav class="fixed w-full h-full" />
+      <nav class="fixed h-full w-full" />
     </div>
   </header>
 </template>
